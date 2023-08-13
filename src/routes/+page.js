a82efc7f2cabc2c54ff2a967f6e13feabfb10511
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-08-13 17:32:01 +0200

    update deps and new api

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
 	try {
-		const res = await fetch(`https://oengus.io/api/marathons/`);
+		const res = await fetch(`https://oengus.io/api/v1/marathons/`);
 		return await res.json();
 	} catch (err) {
 		throw error(404, 'Not found');
--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
 	try {
-		const res = await fetch(`https://oengus.io/api/marathons/${params.slug}/schedule`);
+		const res = await fetch(`https://oengus.io/api/v1/marathons/${params.slug}/schedule`);
 		return await res.json();
 	} catch (err) {
 		throw error(404, 'Not found');
